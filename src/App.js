ff494076fe81cd3b9517881a5c219606494a5b3b
jhs512 <jangka512@gmail.com>
2022-05-23 19:22:22 +0900

    mui 테마 색상을 css 변수화

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ function App() {
   return (
     <>
       <div className="flex-1 flex items-center justify-center">
-        <Button variant="contained">Hello World</Button>
+        <div>
+          <span className="text-[color:var(--mui-color-primary-main)]">
+            안녕하세요.
+          </span>
+          <br />
+          <span className="text-[color:var(--mui-color-primary-dark)]">
+            안녕하세요.
+          </span>
+          <br />
+          <span className="text-[color:var(--mui-color-primary-light)]">
+            안녕하세요.
+          </span>
+        </div>
       </div>
     </>
   );
