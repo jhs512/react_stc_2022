34536855a5cca7273cb7efb833d9bb37c2b1291e
jhs512 <jangka512@gmail.com>
2022-05-23 19:13:47 +0900

    mui 설치

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Button } from "@mui/material";
+
 function App() {
   return (
     <>
       <div className="flex-1 flex items-center justify-center">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button variant="contained">Hello World</Button>
       </div>
     </>
   );
